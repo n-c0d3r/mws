b3bf87ce1e9c5a6e5be26629333e23b96b2213d7
dnblue-code <dntechunity@gmail.com>
2021-05-15 12:51:35 +0700

    Update MWSLandscape Shader

--- Shaders/Subshaders/MWSColorHeightBlend.hlsl
+++ Shaders/Subshaders/MWSColorHeightBlend.hlsl
@@ -1,16 +1,14 @@
-void MWSColorHeightBlend_float(float height0,float height1,bool useColorHeightBlend,float ifNotUseColorHeightBlend,out float SecondLayerStrength) {
+void MWSColorHeightBlend_float(float height0,float height1,float ifNotUseColorHeightBlend,float ColorHeightBlend,out float SecondLayerStrength) {
 	
 	SecondLayerStrength = ifNotUseColorHeightBlend;
 
-	if (useColorHeightBlend) {
-		if (height1 > height0) {
-			SecondLayerStrength = 1;
-		}
-		else {
-			SecondLayerStrength = 0;
-		}
+	if (height1 > height0) {
+		SecondLayerStrength = 1;
 	}
 	else {
-
+		SecondLayerStrength = 0;
 	}
+		
+	SecondLayerStrength = lerp(ifNotUseColorHeightBlend, SecondLayerStrength, ColorHeightBlend);
+	
 }
